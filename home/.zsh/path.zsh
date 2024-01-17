74b3299eb4b5624e231b5649ab6a2833f5455e92
Masanobu Nozawa <masa.nozawa@sslab.ics.keio.ac.jp>
2024-01-17 19:21:59 +0900

    add .zsh/path.zsh

---
+++ home/.zsh/path.zsh
@@ -0,0 +1,23 @@
+# Java
+export JAVA_HOME="$HOME/oss/jdk-20.0.1"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+# Python
+#export PYTHONPATH="/home/nozawa/.asdf/installs/python/3.11.2/lib/python3.11/site-packages/"
+export PYTHONPATH="/home/nozawa/.asdf/installs/python/3.9.7/lib/python3.9/site-packages/:$PYTHONPATH"
+export PYTHONPATH="/home/nozawa/research/mymmap/:$PYTHONPATH"
+export PYTHONPATH="/home/nozawa/research/master-proposal/build/lib.linux-x86_64-3.9/:$PYTHONPATH"
+export CPATH="/home/nozawa/.asdf/installs/python/3.9.7/include/python3.9/:$CPATH"
+export CPATH="/home/nozawa/.asdf/installs/python/3.9.7/lib/python3.9/site-packages/numpy/core/include/:$CPATH"
+export CPATH="/usr/local/lib/python3.9/site-packages/numpy/core/include/:$CPATH"
+export CPATH="/home/nozawa/.local/include/:$CPATH"
+#export CPATH="/home/nozawa/research/master-proposal/include:$CPATH"
+
+# Julia
+export JULIA_DIR="$HOME/.asdf/installs/julia"
+export CPATH="$HOME/.asdf/installs/julia/1.6.2/include/julia/:$CPATH"
+
+# clang's {include, library} path
+export CPATH="/home/nozawa/research/mymmap:$CPATH"
+#export LD_LIBRARY_PATH="/home/nozawa/.local/lib:$LIBRARY_PATH"
+#export LD_LIBRARY_PATH="$CPATH:$LIBRARY_PATH"
