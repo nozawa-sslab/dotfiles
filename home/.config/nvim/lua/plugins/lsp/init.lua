f9b777fb65749496a6605531dc0cddabac81e113
Masanobu Nozawa <masa.nozawa@sslab.ics.keio.ac.jp>
2024-01-22 15:32:22 +0900

    udpate lspsaga to show diagnosis in hover at CursorHold

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -5,8 +5,20 @@ return {
           'nvim-treesitter/nvim-treesitter',
           'nvim-tree/nvim-web-devicons'
         },
+        enabled = true,
         config = function()
-          require('lspsaga').setup({})
+            vim.api.nvim_create_autocmd("CursorHold", {
+                command = "Lspsaga show_cursor_diagnostics ++unfocus",
+            })
+            vim.opt.updatetime = 300
+            vim.diagnostic.config({
+                virtual_text = false
+            })
+            require('lspsaga').setup({
+                symbol_in_winbar = {
+                    enable = false
+                },
+            })
         end,
     }
 }
