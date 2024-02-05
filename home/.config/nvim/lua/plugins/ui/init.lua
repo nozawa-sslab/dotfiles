301df62bebea8f0400cb843805c27e37fc1cc16c
Masanobu Nozawa <masa.nozawa@sslab.ics.keio.ac.jp>
2024-02-05 11:14:43 +0900

    update to use noice.nvim

--- home/.config/nvim/lua/plugins/ui/init.lua
+++ home/.config/nvim/lua/plugins/ui/init.lua
@@ -41,5 +41,31 @@ return {
         }
       }
     end
+  },
+  {
+    'folke/noice.nvim',
+    event = "VeryLazy",
+    dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        messages = {
+          enabled = false,
+        },
+        popupmenu = {
+          enabled = false,
+        }
+      })
+    end
   }
 }
